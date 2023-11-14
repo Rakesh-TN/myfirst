89110b1baedfe153067766e2aefed9936cfe1581
Rakesh-TN <tnrakesh68@outlook.com>
2023-11-14 17:07:24 +0530

    Add project files.

---
+++ myfirst/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace myfirst 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello "); //This Write will take the space of the Content
+            Console.WriteLine("I'm Rakesh"); //This WriteLine will take the space of the Whole Line 
+            Console.Beep();
+        }
+    }
+}
