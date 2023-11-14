fd671ae13a3ffb00445134c410f3736cff92f932
Rakesh-TN <tnrakesh68@outlook.com>
2023-11-14 17:16:57 +0530

    Add

--- myfirst/Program.cs
+++ myfirst/Program.cs
@@ -9,6 +9,16 @@ namespace myfirst
             Console.Write("Hello "); //This Write will take the space of the Content
             Console.WriteLine("I'm Rakesh"); //This WriteLine will take the space of the Whole Line 
             Console.Beep();
+            /*
+             * Escape Sequence with Represents
+             * \a - Bull (alert)
+             * \b - Backspace 
+             * \f - Form feed 
+             * \n - New line 
+             * \r - Carriage return
+             * \t - Horizontal tab
+             * \v - Vertical tab
+             */
         }
     }
 }
